8c8ecd29e1697b956052ccd6bc71723de6e0d3e6
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-10-23 14:25:52 +0300

    Migration tanımlandı

---
+++ HotelProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,23 @@
+﻿using HotelProject.EntityLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=PC309\\SQLEXPRESS;initial catalog=CoreProje2ApiDB;integrated security=true");
+        }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Staff> Staffs { get; set;}
+        public DbSet<Subscribe> Subscribes { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
