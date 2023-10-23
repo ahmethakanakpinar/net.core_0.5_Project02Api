0913b8582da9c73b760bb07100ef240ad1a6b090
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-10-23 15:15:43 +0300

    Business Managerlar eklendi

---
+++ HotelProject.BusinessLayer/Concrete/ServiceManager.cs
@@ -0,0 +1,46 @@
+﻿using HotelProject.BusinessLayer.Abstrack;
+using HotelProject.DataAccessLayer.Abstrack;
+using HotelProject.EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.BusinessLayer.Concrete
+{
+    public class ServiceManager : IServiceService
+    {
+        private readonly IServiceDal _serviceDal;
+
+        public ServiceManager(IServiceDal serviceDal)
+        {
+            _serviceDal = serviceDal;
+        }
+
+        public void TDelete(Service t)
+        {
+            _serviceDal.Delete(t);
+        }
+
+        public Service TGetById(int id)
+        {
+            return _serviceDal.GetByID(id);
+        }
+
+        public List<Service> TGetList()
+        {
+            return _serviceDal.GetList();
+        }
+
+        public void TInsert(Service t)
+        {
+            _serviceDal.Insert(t);
+        }
+
+        public void TUpdate(Service t)
+        {
+            _serviceDal.Update(t);
+        }
+    }
+}
---
+++ HotelProject.BusinessLayer/Concrete/SubscribeManager.cs
@@ -0,0 +1,46 @@
+﻿using HotelProject.BusinessLayer.Abstrack;
+using HotelProject.DataAccessLayer.Abstrack;
+using HotelProject.EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.BusinessLayer.Concrete
+{
+    public class SubscribeManager : ISubscribeService
+    {
+        private readonly ISubscribeDal _subscribeDal;
+
+        public SubscribeManager(ISubscribeDal subscribeDal)
+        {
+            _subscribeDal = subscribeDal;
+        }
+
+        public void TDelete(Subscribe t)
+        {
+            _subscribeDal.Delete(t);
+        }
+
+        public Subscribe TGetById(int id)
+        {
+            return _subscribeDal.GetByID(id);
+        }
+
+        public List<Subscribe> TGetList()
+        {
+            return _subscribeDal.GetList();
+        }
+
+        public void TInsert(Subscribe t)
+        {
+            _subscribeDal.Insert(t);
+        }
+
+        public void TUpdate(Subscribe t)
+        {
+            _subscribeDal.Update(t);
+        }
+    }
+}
