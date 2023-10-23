8e393c5fe19d265c782aa33f25051188e6cbf51b
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-10-23 16:51:56 +0300

    Tema _AdminLayout adlı controller a entegre edildi

---
+++ HotelProject.Web/Controllers/_AdminLayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.Web.Controllers
+{
+    public class _AdminLayoutController : Controller
+    {
+        public IActionResult _AdminLayout()
+        {
+            return View();
+        }
+    }
+}
