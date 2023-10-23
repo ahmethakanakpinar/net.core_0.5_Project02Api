28a43d9b3bde76a80fe0e21d2d5d0f83cb0fa5d3
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-10-23 17:30:18 +0300

    _AdminLayout Partiallara bölündü

--- HotelProject.Web/Controllers/_AdminLayoutController.cs
+++ HotelProject.Web/Controllers/_AdminLayoutController.cs
@@ -8,5 +8,25 @@ namespace HotelProject.Web.Controllers
         {
             return View();
         }
+        public PartialViewResult HeadPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult NavPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult SidebarPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult FooterPartial() 
+        {
+            return PartialView();
+        }
+        public PartialViewResult ScriptsPartial() 
+        {
+            return PartialView();
+        }
     }
 }
