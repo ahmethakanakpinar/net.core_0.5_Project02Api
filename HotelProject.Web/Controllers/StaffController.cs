81402c2857b1fc33f1886760c0a6bab06f9035bb
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-10-24 10:32:38 +0300

    Frontend staff Controller AddStaff kodları yazıldı

--- HotelProject.Web/Controllers/StaffController.cs
+++ HotelProject.Web/Controllers/StaffController.cs
@@ -1,6 +1,7 @@
 ﻿using HotelProject.Web.Models.Staff;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace HotelProject.Web.Controllers
 {
@@ -23,6 +24,24 @@ namespace HotelProject.Web.Controllers
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<List<StaffViewModel>>(jsonData);
                 return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult AddStaff()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddStaff(StaffViewModel staffViewModel)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(staffViewModel);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7113/api/Staff", stringContent);
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
             }
             return View();
         }
