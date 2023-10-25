20a0db3d84afc5a0dcb8d12f57ee1ee8514ba257
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-10-25 17:58:13 +0300

    Frontend ServiceDto için Required hataları yazıldı

--- HotelProject.Web/Dtos/ServiceDto/CreateServiceDto.cs
+++ HotelProject.Web/Dtos/ServiceDto/CreateServiceDto.cs
@@ -1,10 +1,15 @@
-﻿,namespace HotelProject.Web.Dtos.ServiceDto
+﻿,using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.Web.Dtos.ServiceDto
 {
     public class CreateServiceDto
     {
-        public int ServiceID { get; set; }
+        [Required(ErrorMessage = "Hizmet İkon Linki Giriniz.")]
         public string ServiceIcon { get; set; }
+        [Required(ErrorMessage = "Hizmet Başlığı Giriniz.")]
+        [StringLength(50, ErrorMessage ="Hizmet Başlığı En Fazla 50 Karakter Olabilir.")]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Hizmet Açıklaması Giriniz.")]
         public string Description { get; set; }
     }
 }
--- HotelProject.Web/Dtos/ServiceDto/UpdateServiceDto.cs
+++ HotelProject.Web/Dtos/ServiceDto/UpdateServiceDto.cs
@@ -4,10 +4,13 @@ namespace HotelProject.Web.Dtos.ServiceDto
 {
     public class UpdateServiceDto
     {
-        [Required()]
         public int ServiceID { get; set; }
+        [Required(ErrorMessage = "Hizmet İkon Linki Giriniz.")]
         public string ServiceIcon { get; set; }
+        [Required(ErrorMessage = "Hizmet Başlığı Giriniz.")]
+        [StringLength(50, ErrorMessage = "Hizmet Başlığı En Fazla 50 Karakter Olabilir.")]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Hizmet Açıklaması Giriniz.")]
         public string Description { get; set; }
     }
 }
