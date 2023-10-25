155fb63e2bb98901af201da57dbcb2b02b937a93
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-10-25 17:53:44 +0300

    Frontend Service için DTO lar oluşturuldu

---
+++ HotelProject.Web/Dtos/ServiceDto/CreateServiceDto.cs
@@ -0,0 +1,10 @@
+﻿,namespace HotelProject.Web.Dtos.ServiceDto
+{
+    public class CreateServiceDto
+    {
+        public int ServiceID { get; set; }
+        public string ServiceIcon { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ HotelProject.Web/Dtos/ServiceDto/UpdateServiceDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.Web.Dtos.ServiceDto
+{
+    public class UpdateServiceDto
+    {
+        [Required()]
+        public int ServiceID { get; set; }
+        public string ServiceIcon { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
