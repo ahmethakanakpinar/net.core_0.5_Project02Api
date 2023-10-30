320b70ee30708f7c506a5dad59a1bf75d2549dbb
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-10-30 16:34:29 +0300

    About Diğer Katmanlar yazıldı

---
+++ HotelProject.DataAccessLayer/EntityFramework/EfAboutDal.cs
@@ -0,0 +1,19 @@
+﻿using HotelProject.DataAccessLayer.Abstrack;
+using HotelProject.DataAccessLayer.Concrete;
+using HotelProject.DataAccessLayer.Repositories;
+using HotelProject.EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccessLayer.EntityFramework
+{
+    public class EfAboutDal : GenericRepository<About>, IAboutDal
+    {
+        public EfAboutDal(Context context) : base(context)
+        {
+        }
+    }
+}
