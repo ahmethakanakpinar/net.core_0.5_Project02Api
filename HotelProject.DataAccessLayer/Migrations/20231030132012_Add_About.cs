80200c64d332662db69121824af961f893571002
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-10-30 16:22:07 +0300

    About entity yazıldı veritabanı na eklendi

---
+++ HotelProject.DataAccessLayer/Migrations/20231030132012_Add_About.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelProject.DataAccessLayer.Migrations
+{
+    public partial class Add_About : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Abouts",
+                columns: table => new
+                {
+                    AboutID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Title2 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RoomCount = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StaffCount = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ClientCount = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Abouts", x => x.AboutID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Abouts");
+        }
+    }
+}
---
+++ HotelProject.EntityLayer/About.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.EntityLayer
+{
+    public class About
+    {
+        public int AboutID { get; set; }
+        public string Title { get; set;}
+        public string Title2 { get; set;}
+        public string Description { get; set;}
+        public string RoomCount { get; set;}
+        public string StaffCount { get; set;}
+        public string ClientCount { get; set;}
+
+    }
+}
