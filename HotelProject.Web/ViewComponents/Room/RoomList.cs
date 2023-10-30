53238be35474101af34986966baf29ac021a6468
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-10-30 20:24:41 +0300

    ViewComponent Room un View kısmı yazıldı Odalar kısmı Default da tamamlandı

--- HotelProject.Web/ViewComponents/Room/RoomList.cs
+++ HotelProject.Web/ViewComponents/Room/RoomList.cs
@@ -13,14 +13,14 @@ namespace HotelProject.Web.ViewComponents.Room
             _httpClientFactory = httpClientFactory;
         }
 
-        public async Task<IViewComponentResult> Invoke()
+        public async Task<IViewComponentResult> InvokeAsync()
         {
             var client = _httpClientFactory.CreateClient();
             var responseMessage = await client.GetAsync("https://localhost:7113/api/Room");
             if(responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<ResultRoomDto>>(jsonData);
+                var values = JsonConvert.DeserializeObject<List<ResultRoomDto>>(jsonData).Take(3).ToList();
                 return View(values);
             }
             return View();
