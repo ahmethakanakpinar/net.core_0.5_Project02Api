e86d8549a2d1ded7fa5815a11af8dda0d941a3a9
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-10-31 11:29:10 +0300

    Default Staff Component oluşturuldu,

---
+++ HotelProject.Web/ViewComponents/Staff/StaffList.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.Web.ViewComponents.Staff
+{
+    public class StaffList:ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
