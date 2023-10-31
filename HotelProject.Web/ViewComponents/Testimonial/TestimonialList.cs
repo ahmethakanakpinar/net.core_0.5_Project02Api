5a4a2a2bcaa845972f7119a8146e75ade46da7f8
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-10-31 10:50:34 +0300

    Defaultda Testimonial için Api ile verileri çektim

--- HotelProject.Web/ViewComponents/Testimonial/TestimonialList.cs
+++ HotelProject.Web/ViewComponents/Testimonial/TestimonialList.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.Web.Dtos.TestimonialDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HotelProject.Web.ViewComponents.Testimonial
 {
     public class TestimonialList : ViewComponent
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public TestimonialList(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
         public async Task<IViewComponentResult> InvokeAsync()
         {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7113/api/Testimonial");
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
