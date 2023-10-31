983e3d7bb9538c53bfc818d0a42f8ebab2726d96
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-10-31 09:59:06 +0300

    Default da Service için Component oluşturuldu

---
+++ HotelProject.Web/ViewComponents/Service/ServiceList.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.Web.ViewComponents.Service
+{
+    public class ServiceList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
