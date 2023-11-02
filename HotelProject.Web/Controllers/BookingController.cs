a526545a72c7e7eabe574760e72013c032a54e46
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-11-02 18:41:02 +0300

    Booking Rezervasyon tamamlandı api ye veri gidiyor

--- HotelProject.Web/Controllers/BookingController.cs
+++ HotelProject.Web/Controllers/BookingController.cs
@@ -1,11 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.Web.Dtos.BookingDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace HotelProject.Web.Controllers
 {
     public class BookingController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BookingController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        [HttpGet]
         public IActionResult Index()
         {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateBookingDto createBookingDto)
+        {
+            createBookingDto.Status = "Onay Bekleniyor";
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createBookingDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7113/api/Booking", stringContent);
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
             return View();
         }
     }
