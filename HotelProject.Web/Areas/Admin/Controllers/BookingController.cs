daab2e4b4d2bb428559241dd9e33b6617b923e0a
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-11-06 11:09:48 +0300

    Admin Booking Controller Listeleme yazıldı

---
+++ HotelProject.Web/Areas/Admin/Controllers/BookingController.cs
@@ -0,0 +1,29 @@
+﻿using HotelProject.Web.Dtos.BookingDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace HotelProject.Web.Areas.Admin.Controllers
+{
+    public class BookingController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public BookingController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7113/api/Booking");
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBookingDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
