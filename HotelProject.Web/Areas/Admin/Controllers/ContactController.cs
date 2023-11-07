d47514c4bfef4f550fff9b158982db6285d4bc96
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-11-07 18:38:18 +0300

    _contactlayout oluşturuldu ve ReceiverMessage oluşturuldu

---
+++ HotelProject.Web/Areas/Admin/Controllers/ContactController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/[controller]/[action]/{id?}")]
+    public class ContactController : Controller
+    {
+        public IActionResult ReceiverMessage()
+        {
+            return View();
+        }
+    }
+}
