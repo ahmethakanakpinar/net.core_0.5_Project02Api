868b47ea5115e338a6dfe23c88ede8fbfa4380ed
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-11-07 13:49:00 +0300

    Contact entity oluşturuldu

---
+++ HotelProject.EntityLayer/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.EntityLayer
+{
+    public class Contact
+    {
+        public int ContactID { get; set; }
+        public string Sender { get; set; }
+        public string SenderName { get; set; }
+        public string Receiver { get; set; }
+        public string ReceiverName { get; set; }
+        public string Subject { get; set; }
+        public string MessageContent { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
