01b83750fb4ce43c8b603123f91e1b4e04d380a7
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-11-09 16:55:15 +0300

    ContactCategory katmanlar yazıldı

---
+++ HotelProject.BusinessLayer/Concrete/ContactCategoryManager.cs
@@ -0,0 +1,46 @@
+﻿using HotelProject.BusinessLayer.Abstrack;
+using HotelProject.DataAccessLayer.Abstrack;
+using HotelProject.EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.BusinessLayer.Concrete
+{
+    internal class ContactCategoryManager : IContactCategoryService
+    {
+        private readonly IContactCategoryDal _contactCategoryDal;
+
+        public ContactCategoryManager(IContactCategoryDal contactCategoryDal)
+        {
+            _contactCategoryDal = contactCategoryDal;
+        }
+
+        public void TDelete(ContactCategory t)
+        {
+            _contactCategoryDal.Delete(t);
+        }
+
+        public ContactCategory TGetById(int id)
+        {
+            return _contactCategoryDal.GetByID(id);
+        }
+
+        public List<ContactCategory> TGetList()
+        {
+            return _contactCategoryDal.GetList();
+        }
+
+        public void TInsert(ContactCategory t)
+        {
+            _contactCategoryDal.Insert(t);
+        }
+
+        public void TUpdate(ContactCategory t)
+        {
+            _contactCategoryDal.Update(t);
+        }
+    }
+}
