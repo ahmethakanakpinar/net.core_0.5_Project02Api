aff91a23f86ede3845520fd28c82391695313542
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-11-15 10:03:02 +0300

    Aadmin UserController oluşturuldu ve view yazıldı

---
+++ HotelProject.Web/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using HotelProject.EntityLayer;
+using HotelProject.Web.Dtos.UserDto;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace HotelProject.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/[controller]/[action]/{id?}")]
+    public class UserController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public UserController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userManager.Users.ToListAsync(); // Kullanıcı listesini materyalize et
+
+            var modelList = await Task.WhenAll(users.Select(async user => new ResultUserDto
+            {
+                Name = user.Name,
+                Surname = user.Surname,
+                ImageUrl = user.ImageUrl,
+                UserName = user.UserName,
+                PhoneNumber = user.PhoneNumber,
+                Role = (await _userManager.GetRolesAsync(user)).FirstOrDefault()
+            }));
+
+            return View(modelList.ToList());
+        }
+
+    }
+}
---
+++ HotelProject.Web/Dtos/UserDto/ResultUserDto.cs
@@ -0,0 +1,17 @@
+﻿namespace HotelProject.Web.Dtos.UserDto
+{
+    public class ResultUserDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        //public IFormFile? Image { get; set; }
+        public string? ImageUrl { get; set; }
+        public string UserName { get; set; }
+
+        public string PhoneNumber {  get; set; }
+
+        public string Role { get; set; }
+
+    }
+}
