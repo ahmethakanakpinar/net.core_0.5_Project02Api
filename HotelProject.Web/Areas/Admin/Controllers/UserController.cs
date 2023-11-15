64c43807f1d329b70429eb50adc0ca015895328f
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-11-15 18:44:21 +0300

    Admin User Profile kısmı oluşturuldu Controller kısmı yazıldı

--- HotelProject.Web/Areas/Admin/Controllers/UserController.cs
+++ HotelProject.Web/Areas/Admin/Controllers/UserController.cs
@@ -154,5 +154,53 @@ namespace HotelProject.Web.Areas.Admin.Controllers
             }
             return View();
         }
+        [HttpGet]
+            public async Task<IActionResult> Profile(int id)
+            {
+                var values = await _userManager.FindByNameAsync(User.Identity.Name);
+                var user = _mapper.Map<ProfileUserDto>(values);
+
+            return View(user);
+            }
+
+        [HttpPost]
+        public async Task<IActionResult> Profile(ProfileUserDto profileUserDto)
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            if (profileUserDto.Image != null)
+            {
+                var resource = Directory.GetCurrentDirectory();
+                var extension = Path.GetExtension(profileUserDto.Image.FileName);
+                var imagename = Guid.NewGuid() + extension;
+                var savelocation = resource + "/wwwroot/userimage/" + imagename;
+                var stream = new FileStream(savelocation, FileMode.Create);
+                await profileUserDto.Image.CopyToAsync(stream);
+                profileUserDto.ImageUrl = "/userimage/" + imagename;
+                user.ImageUrl = profileUserDto.ImageUrl;
+            }
+
+            user.UserName = profileUserDto.UserName;
+            user.Name = profileUserDto.Name;
+            user.Surname = profileUserDto.Surname;
+            user.City = profileUserDto.City;
+            user.Email = profileUserDto.Email;
+            user.PhoneNumber = profileUserDto.PhoneNumber;
+
+            if (profileUserDto.OldPasswordHash != null)
+            {
+                await _userManager.ChangePasswordAsync(user, profileUserDto.OldPasswordHash, profileUserDto.PasswordHash);
+            }
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "User");
+            }
+            return View();
+        }
     }
 }
---
+++ HotelProject.Web/Dtos/UserDto/ProfileUserDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.Web.Dtos.UserDto
+{
+    public class ProfileUserDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string? City { get; set; }
+        public string? Email { get; set; }
+        public IFormFile? Image { get; set; }
+        public string? ImageUrl { get; set; }
+        public string UserName { get; set; }
+
+        public string? OldPasswordHash { get; set; }
+        public string? PasswordHash { get; set; }
+        [Compare("PasswordHash", ErrorMessage = "Şifreler Uyumlu Değil")]
+        public string? ConfirmPasswordHash { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+}
