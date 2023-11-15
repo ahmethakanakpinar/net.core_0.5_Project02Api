701e5bfe6ca0d5c8412df4f2ff7d4cc56d14da8f
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2023-11-15 20:14:02 +0300

    Admin UserRole Listeleme kısmı tamamlandı

---
+++ HotelProject.Web/Areas/Admin/Controllers/UserRoleController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using HotelProject.BusinessLayer.Abstrack;
+using HotelProject.Web.Dtos.UserRoleDto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("/Admin/[controller]/[action]/{id?}")]
+    public class UserRoleController : Controller
+    {
+        private readonly IAppRoleService _roleService;
+        private readonly IMapper _mapper;
+
+        public UserRoleController(IAppRoleService roleService, IMapper mapper)
+        {
+            _roleService = roleService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var roledata = _roleService.TGetList();
+            var values = _mapper.Map<List<ResultUserRoleDto>>(roledata);
+            return View(values);
+        }
+    }
+}
